1a1701c5b1928cb8988e21f4642c3ecebf1049ed
SpidY21 <yashkumarroy164@gmail.com>
2023-06-27 00:55:34 +0530

    First Testing

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+class car(){
+
+    constructor(model :String) : this() {
+
+    }
+    fun newfun(){
+        println("Good Morning Yash Sir")
+    }
+}
+fun main(args: Array<String>) {
+    println("Hello Yash")
+    val c= car()
+    c.newfun()
+}
